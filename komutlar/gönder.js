52734f1741fb83ebfc47fb6bf5da7196445bab47
Glitch (clooudup) <none>
2020-10-24 10:54:56 +0000

    ⛄️🛫 Checkpoint ./komutlar/gönder.js:22224517/11518

--- komutlar/gönder.js
+++ komutlar/gönder.js
@@ -1,36 +1,31 @@
 const Discord = require("discord.js");
-exports.run = function(client, message, args) {
-const log = message.guild.channels.find(c => c.id === "720651997082288238"); //buraya kayıt log id koyun
-    let type = args.slice(0).join(" ");
-    if (type.length < 1)
-      return message.channel.send(
-        new Discord.MessageEmbed().setDescription("Kullanım: v!sikayet <Şikayet>")
-      );
-    const embed = new Discord.MessageEmbed()
-      .setColor("RANDOM")
-      .setDescription("Şikayetiniz bildirildi.");
-    message.channel.send(embed);
-  
-    const embed2 = new Discord.MessageEmbed()
-    .setAuthor("Kayıt Yapıldı")
-    .addField(`Kaydı yapılan\n`, `${message.user.tag}`)
+const db = require('quick.db');
+exports.run = (client, message, args) => {
+  const log = message.guild.channels.find(c => c.id === "720651997082288238"); //buraya kayıt log id koyun
+    let member = message.mentions.users.first() || client.users.get(args.join(' '))
+      if(!member) return message.channel.send("Bir kullanıcı girin.")
+    const c = message.guild.member(member)
+    const nick = args[1];
+    const yas = args[2];
+      if(!nick) return message.channel.send("Bir isim girin.")
+    const embed = new Discord.RichEmbed()
+    .setAuthor("Yeni Şablon Gönderildi!!")
+    .addField(`Kaydı yapılan\n`, `${c.user.tag}`)
     .addField(`Kaydı yapan\n`, `${message.author.tag}`)
-    .addField(`Yeni isim\n`, `${messa}`)
+    .addField(`Yeni isim\n`, ``)
     .setFooter("RabeL | kayıt sistemi")
     .setColor("#000000")
-    log.send(embed2)
-};
-
+    log.send(embed)
+  }
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["report"],
-  permLevel: 0,
-  kategori: "genel"
+  aliases: ["k"],
+  permLevel: 0
 };
-
 exports.help = {
-  name: "şikayet",
-  description: "Kullanıcıyı şikayet eder.",
-  usage: "şikayet <Şikayet>"
+  name: "k",
+  description: "k",
+  usage: "k"
 };
+   
