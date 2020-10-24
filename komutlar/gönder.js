1e99236ad7bb06ce293525fd8158b1c63bdcc5c9
Glitch (clooudup) <none>
2020-10-24 14:24:36 +0000

    🍈😻 Checkpoint ./komutlar/gönder.js:22224517/254

--- komutlar/gönder.js
+++ komutlar/gönder.js
@@ -14,12 +14,12 @@ exports.run = (client, message, args) => {
 
     const embed = new Discord.MessageEmbed()
     .setAuthor("Yeni Şablon Gönderildi!!")
-    .addField('Şablonu Gönderen \n', '<@!${message.author.user.id}>')
+    .addField("Şablonu Gönderen \n", "<@!${message.author.user.id}>")
     .addField(`Gönderilen Şablon\n`, `${nick}`)
     .addField(`Açıklama \n`, `${yas}`)
     .setFooter("Serves Template | Hazır Şablon sistemi")
     .setColor("#000000")
-    client.channel.cache.get("769217541951586307").send(embed);
+    client.channels.cache.get("769217541951586307").send(embed)
   }
 exports.conf = {
   enabled: true,
