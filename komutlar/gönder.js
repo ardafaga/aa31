f4887509efeb98c4eec29759d9b44b2de763b23d
Glitch (clooudup) <none>
2020-10-24 14:13:19 +0000

    🎎💒 Checkpoint ./komutlar/gönder.js:22224517/692 ./.glitch-assets:22224517/481

--- komutlar/gönder.js
+++ komutlar/gönder.js
@@ -2,33 +2,34 @@ const Discord = require("discord.js");
 const db = require('quick.db');
 exports.run = (client, message, args) => {
 
-    const gönderildi = new Discord.MessageEmbed()
-    .setAuthor("Yeni Şablon Gönderildi!!")
-    .addfield
-    .setColor("#000000")
-    message.channel.send(gönderildi)
- 
     const nick = args[1];
     const yas = args[2];
-      if(!nick) return message.channel.send("Bir isim girin.")
+    if(!nick) return message.channel.send("Bir Şablon girin.")
+  
+    const gönderildi = new Discord.MessageEmbed()
+    .setAuthor("Yeni Şablon Gönderildi!!")
+    .setDescription(`Desteklerinizden Dolayı Teşekkür Ederiz. Uygun görülürse Bota Eklenicektir.`)
+    .setColor("#2bfe0f")
+    message.channel.send(gönderildi)
+
     const embed = new Discord.MessageEmbed()
     .setAuthor("Yeni Şablon Gönderildi!!")
-    .addField(`Şablonu Gönderen \n`, `<@!${message.author.user.id}>`)
+    .addField('Şablonu Gönderen \n', '<@!${message.author.user.id}>')
     .addField(`Gönderilen Şablon\n`, `${nick}`)
     .addField(`Açıklama \n`, `${yas}`)
-    .setFooter("RabeL | kayıt sistemi")
+    .setFooter("Serves Template | Hazır Şablon sistemi")
     .setColor("#000000")
-    client.channels.get("769217541632557094").send(embed);
+    client.channel.cache.get("769217541951586307").send(embed);
   }
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["k"],
+  aliases: ["g","send"],
   permLevel: 0
 };
 exports.help = {
-  name: "k",
+  name: "gönder",
   description: "k",
-  usage: "k"
+  usage: "gönder"
 };
    
