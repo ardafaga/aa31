7fafec3fadfcc8c5915624b61c87bb29abeefb38
Glitch (clooudup) <none>
2020-10-24 14:47:50 +0000

    🐯🕌 Checkpoint ./komutlar/gönder.js:22224517/221

--- komutlar/gönder.js
+++ komutlar/gönder.js
@@ -2,9 +2,10 @@ const Discord = require("discord.js");
 const db = require('quick.db');
 exports.run = (client, message, args) => {
 
-    const nick = args[1];
-    const yas = args[2];
-    if(!nick) return message.channel.send("Bir Şablon girin.")
+    const link = args[0];
+    const açıklama = args.slice(1).join(" ") 
+    
+    if(!link) return message.channel.send("Bir Şablon girin.")
   
     const gönderildi = new Discord.MessageEmbed()
     .setAuthor("Yeni Şablon Gönderildi!!")
@@ -15,8 +16,8 @@ exports.run = (client, message, args) => {
     const embed = new Discord.MessageEmbed()
     .setAuthor("Yeni Şablon Gönderildi!!")
     .addField("Şablonu Gönderen \n", message.author)
-    .addField(`Gönderilen Şablon\n`, `${nick}`)
-    .addField(`Açıklama \n`, `${yas}`)
+    .addField(`Gönderilen Şablon\n`, `${link}`)
+    .addField(`Açıklama \n`, `${açıklama}`)
     .setFooter("Serves Template | Hazır Şablon sistemi")
     .setColor("#000000")
     client.channels.cache.get("769217541951586307").send(embed)
