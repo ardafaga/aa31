68bde03b3508b94cf760dee50a5071f222b2888c
Glitch (clooudup) <none>
2020-10-24 13:39:01 +0000

    📠🍿 Checkpoint ./komutlar/gönder.js:22224517/68

--- komutlar/gönder.js
+++ komutlar/gönder.js
@@ -1,20 +1,19 @@
 const Discord = require("discord.js");
 const db = require('quick.db');
 exports.run = (client, message, args) => {
-  const log = message.guild.channels.find(c => c.id === "769217541632557094"); //buraya kayıt log id koyun
+const log = message.guild.channels.find.id("769217541632557094"); //buraya kayıt log id koyun
 
-      const gönderildi = new Discord.MessageEmbed()
+    const gönderildi = new Discord.MessageEmbed()
     .setAuthor("Yeni Şablon Gönderildi!!")
     .setColor("#000000")
     message.channel.send(gönderildi)
-  
-    const c = message.author
+ 
     const nick = args[1];
     const yas = args[2];
       if(!nick) return message.channel.send("Bir isim girin.")
     const embed = new Discord.MessageEmbed()
     .setAuthor("Yeni Şablon Gönderildi!!")
-    .addField(`Şablonu Gönderen \n`, `<@!${c.user.id}>`)
+    .addField(`Şablonu Gönderen \n`, `<@!${message.author.user.id}>`)
     .addField(`Gönderilen Şablon\n`, `${nick}`)
     .addField(`Açıklama \n`, `${yas}`)
     .setFooter("RabeL | kayıt sistemi")
