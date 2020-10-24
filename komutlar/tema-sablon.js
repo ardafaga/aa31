14f6661a46087cda4847431c06aa4fdb03d6d464
Glitch (clooudup) <none>
2020-10-24 19:59:25 +0000

    😺🏝 Checkpoint ./komutlar/tema-sınıf.js:22224517/1114 ./komutlar/tema-sablon.js:22224517/1085 ./komutlar/tema-dil-deişen-sunucu.js:22224517/1210 ./komutlar/tema-nivotv.js:22224517/1114 ./komutlar/tema-gelişmiş-satış.js:22224517/1103 ./komutlar/tema-j4jv2.js:22224517/1061 ./komutlar/tema-bot-satış.js:22224517/12 ./komutlar/tema-j4j.js:22224517/12

--- komutlar/tema-sablon.js
+++ komutlar/tema-sablon.js
@@ -3,16 +3,16 @@ const data = require('quick.db');
 const ms = require('ms');
 
 exports.run = async (client, message, args) => {
-const datas = await data.fetch(`${message.author.id}.zaman.j4j`);
+const datas = await data.fetch(`${message.author.id}.zaman.şablon`);
 if(Date.now() < datas) return message.channel.send(new Discord.MessageEmbed().setColor('#00001').setDescription(`${message.author} **__6__ saat de bir kullanabilirsiniz!**`)).then(m => m.delete({timeout: 6000}));
-data.set(`${message.author.id}.zaman.j4j`, Date.now()+ms('6h'));
+data.set(`${message.author.id}.zaman.şablon`, Date.now()+ms('6h'));
 message.channel.send(new Discord.MessageEmbed()
-.setDescription(`${message.author} **Özelden gönderdim __j4j sunucunu__ gönderdim bakar mısın.**`));
-message.author.send(`> **1 adet __J4J__ sunucusu kurmak için tema geldi.
+.setDescription(`${message.author} **Özelden gönderdim __Şablon Teması__ gönderdim bakar mısın.**`));
+message.author.send(`> **1 adet __Şablon__ sunucusu kurmak için tema geldi.
 
 > Güle güle kullan 😇
 
-https://discord.new/EPenkmEt3G2G`).catch(error => message.channel.send(new Discord.MessageEmbed().setDescription("Mesajı gönderemedim. Muhtemelen DM'n kapalı.")));
+https://discord.new/`).catch(error => message.channel.send(new Discord.MessageEmbed().setDescription("Mesajı gönderemedim. Muhtemelen DM'n kapalı.")));
 };
 exports.conf = {
   enabled: true,
@@ -22,5 +22,5 @@ exports.conf = {
 }
 
 exports.help = {
-  name: 'j4j-tema'
+  name: 'şablon-tema'
 };
