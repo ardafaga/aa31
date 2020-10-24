ea9bccc81887f188e8bb54ffa11680bedc9d631c
Glitch (clooudup) <none>
2020-10-24 13:15:56 +0000

    🎰🏛 Checkpoint ./komutlar/gönder.js:22224517/1256

--- komutlar/gönder.js
+++ komutlar/gönder.js
@@ -1,18 +1,22 @@
 const Discord = require("discord.js");
 const db = require('quick.db');
 exports.run = (client, message, args) => {
-  const log = message.guild.channels.find(c => c.id === "720651997082288238"); //buraya kayıt log id koyun
-    let member = message.mentions.users.first() || client.users.get(args.join(' '))
-      if(!member) return message.channel.send("Bir kullanıcı girin.")
-    const c = message.guild.member(member)
+  const log = message.guild.channels.find(c => c.id === "769217541632557094"); //buraya kayıt log id koyun
+
+      const gönderildi = new Discord.MessageEmbed()
+    .setAuthor("Yeni Şablon Gönderildi!!")
+    .setColor("#000000")
+    message.channel.send(gönderildi)
+  
+    const c = message.author
     const nick = args[1];
     const yas = args[2];
       if(!nick) return message.channel.send("Bir isim girin.")
-    const embed = new Discord.RichEmbed()
+    const embed = new Discord.MessageEmbed()
     .setAuthor("Yeni Şablon Gönderildi!!")
     .addField(`Şablonu Gönderen \n`, `<@!${c.user.id}>`)
-    .addField(`Kaydı yapan\n`, `${message.author.tag}`)
-    .addField(`Yeni isim\n`, ``)
+    .addField(`Gönderilen Şablon\n`, `${nick}`)
+    .addField(`Açıklama \n`, `${yas}`)
     .setFooter("RabeL | kayıt sistemi")
     .setColor("#000000")
     log.send(embed)
